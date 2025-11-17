7aaf097a1efdd824344504e2cc6b93f27838f777
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-11-18 08:50:32 +0900

    feat: add Active Storage

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,6 @@
 class Recipe < ApplicationRecord
+  has_one_attached :image
+  
   validates :image, presence: true
   validates :name, presence: true
   validates :description, presence: true, length: { maximum: 1000 }
