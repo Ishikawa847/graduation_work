d78564b6fd2913783d96921429942c0b841629e0
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-11-19 08:28:08 +0900

    edit migtarion file

--- db/migrate/20251117232238_create_recipes.rb
+++ db/migrate/20251117232238_create_recipes.rb
@@ -1,7 +1,14 @@
 class CreateRecipes < ActiveRecord::Migration[7.2]
   def change
     create_table :recipes do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.references :user, null: false, foreign_key: true
+
       t.timestamps
     end
+
+    add_index :recipes, :name
+    add_index :recipes, :created_at
   end
 end
