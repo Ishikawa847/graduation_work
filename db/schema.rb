31df60019d6820882d44657086232e7148005bc5
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-11-23 21:40:14 +0900

    feat: create Ingredient model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_18_233329) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_23_123448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,12 +42,21 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_18_233329) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.float "protein"
+    t.float "fat"
+    t.float "carb"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "recipes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name", null: false
     t.text "description", null: false
     t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["created_at"], name: "index_recipes_on_created_at"
     t.index ["name"], name: "index_recipes_on_name"
     t.index ["user_id"], name: "index_recipes_on_user_id"
