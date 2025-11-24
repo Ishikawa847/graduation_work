09e7fccc4bd455e680a5d1a2a58d8668ab96c3a8
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-11-24 20:03:48 +0900

    feat: edit new and create in recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,6 +4,27 @@ class RecipesController < ApplicationController
   end
 
   def new
-    @recipe = Recipe.new
+    @recipe = current_user.recipes.build
+    # 材料入力用のフォームを3つ
+    3.times { @recipe.recipe_ingredients.build }
+  end
+
+  def create
+    @recipe = current_user.recipes.build(recipe_params)
+    
+    if @recipe.save
+      redirect_to @recipe, notice: 'レシピが投稿されました！'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :name, :description, :image,
+      recipe_ingredients_attributes: [:id, :ingredient_id, :quantity, :_destroy]
+    )
   end
 end
