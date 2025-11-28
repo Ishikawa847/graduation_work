6dc1b84c0e940824414672508414c2b7a51f1eed
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-11-29 08:49:14 +0900

    feat: add profile controller

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,37 @@
+class ProfilesController < ApplicationController
+  before_action :require_login  # ログイン必須
+  
+  # プロフィール表示
+  def show
+    @user = current_user
+    @recipes = current_user.recipes
+  end
+  
+  # プロフィール編集画面
+  def edit
+    @user = current_user
+  end
+  
+  # プロフィール更新
+  def update
+    @user = current_user
+    
+    if @user.update(profile_params)
+      redirect_to profile_path, success: 'プロフィールを更新しました'
+    else
+      flash.now[:danger] = 'プロフィールの更新に失敗しました'
+      render :edit, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def profile_params
+    params.require(:user).permit(
+      :height,
+      :weight,
+      :age,
+      :gender
+    )
+  end
+end
