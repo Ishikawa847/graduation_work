7b1846b5f31cdf3ee01438cfbf8f2120a34600ab
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-12-06 21:29:42 +0900

    feat: add validation to menu model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,3 +1,7 @@
 class Menu < ApplicationRecord
   belongs_to :user
+  has_many :menu_recipes, dependent: :destroy
+  has_many :recipes, through: :menu_recipes
+
+  validates :name, presence: true, length: { maximum: 255 }
 end
