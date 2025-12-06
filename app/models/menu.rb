5add2ea7216685fb5b61da8d82af3733761ddef7
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-12-06 23:53:11 +0900

    feat: nested atrribute for menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,5 +3,7 @@ class Menu < ApplicationRecord
   has_many :menu_recipes, dependent: :destroy
   has_many :recipes, through: :menu_recipes
 
+  accepts_nested_attributes_for :menu_recipes, allow_destroy: true
+
   validates :name, presence: true, length: { maximum: 255 }
 end
