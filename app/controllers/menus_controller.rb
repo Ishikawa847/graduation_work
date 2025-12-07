6a9b8a653f3482336330f8236d3f02386f6c8bba
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-12-07 20:38:20 +0900

    feat: make controller action for menu

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,6 +2,8 @@ class MenusController < ApplicationController
 
   def new
     @menu = current_user.menus.build
+    3.times { @menu.menu_recipes.build }
+    @recipes = Recipe.all
   end
   
   def create
@@ -10,8 +12,18 @@ class MenusController < ApplicationController
     if @menu.save
       redirect_to @menu, success: 'メニューを作成しました'
     else
+      @recipes = Recipe.all
       flash.now[:danger] = 'メニューの作成に失敗しました'
       render :new, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(
+      :name,
+      menu_recipes_attributes: [:id, :recipe_id, :position, :_destroy]
+    )
   end
 end
