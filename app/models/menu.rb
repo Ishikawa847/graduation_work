a5ead276a069a6638541a659baed00b1784b7089
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2025-12-17 15:46:03 +0900

    feat: pfc calculation logic for menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -6,4 +6,19 @@ class Menu < ApplicationRecord
   accepts_nested_attributes_for :menu_recipes, allow_destroy: true, reject_if: :all_blank
 
   validates :name, presence: true, length: { maximum: 255 }
+
+  def total_protein
+    recipes.joins(:recipe_ingredients, :ingredients)
+    .sum('ingredients.protein * recipe_ingredients.quantity / 100.0')
+  end
+
+  def total_fat
+    recipes.joins(:recipe_ingredients, :ingredients)
+    .sum('ingredients.fat * recipe_ingredients.quantity / 100.0')
+  end
+
+  def total_carb
+    recipes.joins(:recipe_ingredients, :ingredients)
+    .sum('ingredients.carb * recipe_ingredients.quantity / 100.0')
+  end
 end
