c3a5b0c974c9bcc4746c854c42398257808a9a3d
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-01-04 00:49:18 +0900

    feature: add image delete button

--- app/javascript/controllers/image_preview_controller.js
+++ app/javascript/controllers/image_preview_controller.js
@@ -1,10 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input", "preview"]
+  static targets = [
+    "input",
+    "preview",
+    "removeButton"
+  ]
+  
   connect() {
-    console.log("✅ image-preview controller connected!") // ←追加
+    console.log("image-preview controller connected!") // ←追加
   }
+  
   preview() {
     const file = this.inputTarget.files[0]
     
@@ -13,10 +19,20 @@ export default class extends Controller {
       
       reader.onload = (e) => {
         this.previewTarget.src = e.target.result
-        this.previewTarget.style.display = "block"
+        this.previewTarget.classList.remove('hidden')
+
+        this.removeButtonTarget.classList.remove('hidden')
       }
       
       reader.readAsDataURL(file)
     }
+  }
+
+  remove () {
+    this.inputTarget.value = ''
+    this.previewTarget.src = ""
+    this.previewTarget.classList.add('hidden')
+
+    this.removeButtonTarget.classList.add('hidden')    
   }
 }
