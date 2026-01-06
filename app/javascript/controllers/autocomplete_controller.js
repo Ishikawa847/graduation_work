947a81c7f46bc236947f66f61289d05dfd90e87d
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-01-06 11:21:41 +0900

    feat: implement autocomplete controller logic

--- app/javascript/controllers/autocomplete_controller.js
+++ app/javascript/controllers/autocomplete_controller.js
@@ -2,6 +2,56 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="autocomplete"
 export default class extends Controller {
+  static targets = ["inputs", "results"]
   connect() {
+    this.timeout = null
+  }
+
+  search() {
+    clearTimeout(this.timeout)
+
+    const query = this.inputTarget.value
+
+    if (query.length === 0) {
+      this.hideResults()
+      renturn
+    }
+
+    this.timeout = setTimeout(() =>{
+      fetch('/recipes/autocomplete?q[title_or_body_cont]=${query}')
+      .then(response => response.json)
+      .then(data =>{
+        this.displayResults(data)
+      })
+    }, 300)
+  }
+
+  displayResults(results) {
+    if (results.length === 0) {
+      this.hideResults()
+      return
+    }
+
+    this.resultsTarget.innerHTML = results.map(result => {
+      return
+        <li data-aciton="click->autocomplete#select" data-id="${result.id}">
+          ${result.title}
+        </li>
+    }).join('')
+
+    this.showResults()
+  }
+
+  select(event) {
+    const id = event.target.dataset.id
+    window.location.href = '/recipes/${id}'
+  }
+
+  showResults() {
+    this.resultsTarget.style.display = 'block'
+  }
+
+  hideResults() {
+    this.resultsTarget.style.display = 'none'
   }
 }
