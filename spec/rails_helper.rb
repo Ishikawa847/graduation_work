912cfbc6fa53a461ceb1c987fd780de928a402f2
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-03-03 20:24:56 +0900

    test: add settings to rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -11,6 +11,23 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 # return unless Rails.env.test?
 require 'rspec/rails'
 
+require "capybara/rspec"
+
+Capybara.register_driver :headless_chrome do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless=new") # Chrome 112 以降
+  options.add_argument("--disable-gpu")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1400,1400")
+
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: options
+  )
+end
+
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
