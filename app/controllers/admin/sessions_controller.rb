59a92a00b51cb65daca7813e04d0404ca0d553bf
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-03-17 16:05:19 +0900

    feat: make session controller for admin

---
+++ app/controllers/admin/sessions_controller.rb
@@ -0,0 +1,22 @@
+class Admin::SessionsController < Devise::SessionsController
+
+  def new
+  end
+
+  def create
+    self.resource = warden.authenticate!(auth_options)
+
+    if resource.admin?
+      sign_in(resource_name, resource)
+      redirect_to admin_root_path, notice: "管理者としてログインしました"
+    else
+      sign_out(resource)
+      redirect_to admin_login_path, alert: "管理者ではありません"
+    end
+  end
+
+  def destroy
+    sign_out(resource_name)
+    redirect_to admin_login_path, notice: "ログアウトしました"
+  end
+end
