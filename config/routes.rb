241bb57f5d8a35eafbf6daf91c4949cfa718e5d2
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-03-17 17:59:55 +0900

    feat: add recipe routes for admin

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
+    resources :recipes, only: [:index, :new, :create, :edit, :update, :destroy]
   end
   end
 end
