4e70ef64cbd9389a384c4e187597c1b482e6cbbe
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-03-25 20:11:52 +0900

    feat: add validations to daily menu model

--- app/models/daily_menu.rb
+++ app/models/daily_menu.rb
@@ -1,4 +1,16 @@
 class DailyMenu < ApplicationRecord
   belongs_to :user
   belongs_to :menu
+
+  validates :date, presence: true
+  validates :user_id, uniqueness: { scope: :date }
+  
+  validate :menu_belongs_to_user
+
+  private
+
+  def menu_belongs_to_user
+    return if menu.user_id == user_id
+    errors.add(:menu, "は自分の献立ではありません")
+  end
 end
