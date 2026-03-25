fbbf592c24b5f3671657a64a644503ffd8cb0852
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-03-25 15:06:33 +0900

    feat: make dailymenu model

---
+++ db/migrate/20260325055743_create_daily_menus.rb
@@ -0,0 +1,13 @@
+class CreateDailyMenus < ActiveRecord::Migration[7.2]
+  def change
+    create_table :daily_menus, id: :uuid do |t|
+      t.references :user, null: false, foreign_key: true, type: :uuid
+      t.references :menu, null: false, foreign_key: true, type: :uuid
+      t.date :date
+
+      t.timestamps
+    end
+
+    add_index :daily_menus, [:user_id, :date], unique: true
+  end
+end
---
+++ spec/factories/daily_menus.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :daily_menu do
+    user { nil }
+    menu { nil }
+    date { "2026-03-25" }
+  end
+end
