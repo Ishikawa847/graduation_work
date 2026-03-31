39c9f365ed4328c25cc5ee2c204300e874573db5
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-03-31 16:19:16 +0900

    feat: add date method to DailyMenu

--- app/models/daily_menu.rb
+++ app/models/daily_menu.rb
@@ -7,10 +7,15 @@ class DailyMenu < ApplicationRecord
 
   validate :menu_belongs_to_user
 
+  def start_time
+    date.in_time_zone
+  end
+
   private
 
   def menu_belongs_to_user
     return if menu.blank? || menu.user_id == user_id
     errors.add(:menu, "は自分の献立ではありません")
   end
+  
 end
