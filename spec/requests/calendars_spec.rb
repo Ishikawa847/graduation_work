2d2da209c58067ca2c7208204f804481f2c11de4
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-04-06 23:05:33 +0900

    style: exec rubocop

--- spec/requests/calendars_spec.rb
+++ spec/requests/calendars_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "Calendars", type: :request do
     it "週間カレンダーが表示される" do
       get "/calendar/week"
       expect(response).to have_http_status(:ok)
-      expect(response.body).to include("week") 
+      expect(response.body).to include("week")
     end
   end
 end
