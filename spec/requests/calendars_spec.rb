bf353f8dff587b823636c247d7ab8110e7b57d4d
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-04-06 23:04:29 +0900

    test: add request spec for week caledndar

---
+++ spec/requests/calendars_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "Calendars", type: :request do
+  describe "GET /calendar/week" do
+    it "週間カレンダーが表示される" do
+      get "/calendar/week"
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include("week") 
+    end
+  end
+end
