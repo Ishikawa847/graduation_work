f284c3fc365c58c125fd67f715cbaff215163ea6
ishikawa seiichirou <ishi@ishikawanoMacBook-Air.local>
2026-04-07 21:32:27 +0900

    feat: add logoc to week action

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,5 +5,11 @@ class CalendarsController < ApplicationController
   end
 
   def week
+    @start_date = params[:start_date]&.to_date || Date.today
+    @week_dates = (@start_date.beginning_of_week..@start_date.end_of_week).to_a
+
+    @daily_menus = DailyMenu
+      .includes(:menu)
+      .where(start_time: @week_dates.first.beginning_of_day..@week_dates.last.end_of_day)
   end
 end
